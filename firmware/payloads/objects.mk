0e6e97b01d2929251a8d97f3725b4790c405ecff
KireinaHoro <i@jsteward.moe>
2020-06-13 18:17:52 +0800

    add zcu102 flat kernel

--- firmware/payloads/objects.mk
+++ firmware/payloads/objects.mk
@@ -9,6 +9,7 @@
 
 firmware-bins-$(FW_PAYLOAD) += payloads/test.bin
 firmware-bins-$(FW_PAYLOAD) += payloads/u-boot.bin
+firmware-bins-$(FW_PAYLOAD) += payloads/kernel-flat.bin
 
 test-y += test_head.o
 test-y += test_main.o
@@ -26,3 +27,8 @@ u-boot_bin=$(platform_build_dir)/firmware/payloads/u-boot.bin
 $(u-boot_bin):
 	$(MAKE) -C $(u-boot_root)
 	cp $(u-boot_root)/u-boot.bin $@
+
+kernel_root=$(src_dir)/../../linux
+kernel_bin=$(platform_build_dir)/firmware/payloads/kernel-flat.bin
+$(kernel_bin):
+	cp $(kernel_root)/arch/riscv/boot/Image $@
---
+++ platform/zcu102/config.mk
@@ -0,0 +1,25 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright (c) 2019 Western Digital Corporation or its affiliates.
+#
+# Authors:
+#   Pengcheng Xu <i@jsteward.moe>
+#
+
+# Compiler flags
+platform-cppflags-y =
+platform-cflags-y =
+platform-asflags-y =
+platform-ldflags-y =
+
+# Blobs to build
+FW_TEXT_START=0x800000000
+FW_DYNAMIC=n
+FW_JUMP=n
+FW_PAYLOAD=y
+#FW_PAYLOAD_PATH=$(platform_build_dir)/firmware/payloads/u-boot.bin
+FW_PAYLOAD_PATH=$(platform_build_dir)/firmware/payloads/kernel-flat.bin
+# outside PMP0 zone
+FW_PAYLOAD_OFFSET=0x20000
+FW_PAYLOAD_FDT=zcu102.dtb
